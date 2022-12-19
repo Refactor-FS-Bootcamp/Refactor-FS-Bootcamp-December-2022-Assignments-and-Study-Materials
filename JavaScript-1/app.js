7d74a28f4bbdb419ac4da69177f959e25afd573c
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2022-12-19 20:30:55 +0530

    Add files via upload

---
+++ JavaScript-1/app.js
@@ -0,0 +1,35 @@
+//js - manipulate html and css. 
+
+//js - 1995 - scripting language - 10 days - Brendien Eich 
+
+//scipting langauge - browser .
+
+//javascript - no -  java - schema.
+
+
+//what is the need of scripting lanaguage :
+
+// web1  (satic website ) - web2() -web3() 
+
+//js - scripting - client side 
+// web  - fromtend - backend
+    //    UI  -    SERVER - PROMO - DATABASE.
+
+
+    // JS - FRONEND   // BROWESER - JS 
+    // JS BACKED - // SEVER -RAHNN DHAL  - RUNTIME - BACKEDM
+
+
+//HOW 
+
+// JS CODE (EGLISH)- NOPE - BROWSER
+
+// ..ENGLINE JS - TRANSLATOR - 
+
+
+//  JS  - ENGINE V8 - GOOGLE - 100%
+        // - SPIDER - FF   - 99.9%
+
+// ECMAsCRIPT - RULES AND RUGALTION - GUDIES
+         
+
