148f3d904c993aa09e5c5ef5cddbbbc4368ad202
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2022-12-21 20:04:22 +0530

    Add files via upload

---
+++ JavaScript-3/day3.js
@@ -0,0 +1,99 @@
+//methods
+
+
+//functions
+
+//block of code
+
+// syntax
+function game(){
+    let sum = 4+7;
+    return sum;
+}
+
+const play = function(){
+    return 'start';
+}
+
+console.log(play())
+
+// console.log(game())
+
+//function with aruguments
+// let num1 = 10;
+// let num2 = 12;
+
+
+// function add(a,b){
+//     let sum = a+b;
+//     let mul = a*b;
+//     return [sum,mul];
+// }
+
+// const result = add(num1,num2);
+// const addition = result[1];
+// console.log(addition)
+
+
+
+// methods()
+// object - data and behaviour;
+
+// const person = {
+//     //property
+//     //key : value,
+//     name:'John',
+//     age:45,
+
+//     //methods
+//     detail:function b(){
+//         return 'Employe name is '+person.name+ ' age is '+person.age;
+//     }
+// }
+
+// console.log(person)
+
+// const arr = [12,,2,32,3,32];
+// console.log(arr.pop())
+// console.log(arr.push('hari'))
+
+// console.log(arr)
+
+// let c = 'hello';
+
+// console.log(c.)
+
+//condtional sataments- flow control;
+
+// const coin = 'tail';
+
+// if(coin === 'head'){
+//    console.log('bat')
+// }else{
+//    console.log('Bowl')
+// }
+const demo = document.getElementById('demo')
+
+function color(){
+    console.log('color')
+    let trafficLight= 'yellow';
+
+    if (trafficLight === 'red'){
+        demo.style.backgroundColor = 'red';
+    }else if(trafficLight === 'green'){
+        demo.style.backgroundColor = 'green';
+    }else if(trafficLight === 'yellow'){
+        demo.style.backgroundColor = 'yellow';
+    }else{
+        demo.style.backgroundColor = 'grey';
+    }
+   
+}
+
+
+
+
+
+//operator == / === 
+
+1 === '1' //true
