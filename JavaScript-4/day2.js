a1da55c8357992c8588f55ba9c708e9c7ba0c4bc
nbrefactor <117144208+nbrefactor@users.noreply.github.com>
2022-12-23 19:52:09 +0530

    Add files via upload

---
+++ JavaScript-4/day2.js
@@ -0,0 +1,114 @@
+//variables - contaniter - value.
+
+//decalation variable: 3 keyword, indentifier , value.
+
+// keyword - reserved word let const var.
+
+// indentifer - nameing the varibale.
+
+//value - information .
+
+// let firstName = 'Vikram'
+
+// //debugging 
+// console.log(firstName);
+
+
+// var - bug 1995 no longer;
+
+// let name  = 89;
+// name = 'vell'
+
+// console.log(Name)
+
+
+
+
+
+// name='vell'
+//memory heap;
+
+
+
+//console.log(name)
+//callstack
+
+
+//datatypes -
+// pimitive datatyeps
+// reference datatypes-object
+
+
+//primitive datatypes:
+//string, number, boolean, unefined, null; 
+
+// string - '' squence of characte' - '',"",``;
+// typeof
+
+// let name = '89'
+
+// console.log(typeof name)
+
+// -number - 12, 1.4 0 -12,7
+
+// let num = 1.2;
+// console.log(typeof num)
+
+// let  bool = true;
+
+// console.log(typeof bool)
+
+// let a=undefined;
+// console.log(typeof a)
+
+// let empt = null;
+
+// console.log(typeof null);
+
+//reference - object - Array
+
+// Array = list of values;
+
+// const car = ['audi',23,true,undefined,null,8];
+// console.log(car);
+
+
+// console.log('my car is '+car[1])
+
+// basic - dom
+const car ={
+    name:'audi',
+    age:12,
+    year:2009
+}
+
+// console.log()
+
+const emplyoee={
+    person1:{
+        name:'vikram',
+        age:12,
+    },
+    peson2:{
+        name:'vimal',
+        age:21,
+    },
+
+    list:[1,2,3,12,32,4,3,4,34,5]
+}
+
+
+
+const emplyoee1={
+    person1:{
+        name:'',
+        age:12,
+
+    },
+    peson2:{
+        name:'vimal',
+        age:21,
+    },
+
+    list:[1,2,3,12,32,4,3,4,34,5]
+}
---
+++ JavaScript-4/day4.js
@@ -0,0 +1,65 @@
+//switch - checking multipe condtions
+
+
+// let color = 'red';
+
+// switch (color){
+   
+//     case 'green':
+//          console.log('Go')
+//         break;
+//     case 'red':
+//          console.log('Stop')
+//         break;
+
+
+//     default:
+//          console.log('Unknown')
+
+// }
+
+//loops - console.log('Js is Weird)
+
+//for loop - 
+
+// const a = [12,23,234,23,43,5,4,54,6546,56,546,5,65,56,76,,76,8,67867,8,67,867,9]
+// console.log(a)
+
+//the prcie value is 12.
+//the prcie value is 23.
+
+// let b =[]
+// console.log(b)
+// for(let i=0; i<a.length;i++){
+//   b.push(a[i]*100) 
+// }
+
+// console.log(b)
+
+// let i =0;
+
+// while(i<100){
+//     console.log('hello');
+//     i++;
+// }
+
+//ini
+//condtions
+//code
+//incree
+
+// let i=0
+// do{
+//   console.log(i)
+//   i++;
+// }while(i <10)
+
+//init
+//code to excute;
+//incree
+//condition
+
+
+//basic - Dom
+
+//
